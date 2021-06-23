d23242193fb979b75848f6e2d47777629fbf1db4
Huy Vu Van <huyvu8051@gmail.com>
2021-06-23 23:58:22 +0700

    Create client form for register process

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,33 +1,45 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <p>{{ message }}</p>
-    <input type="email" name="email" v-model="email" id="email" />
-    <input type="password" name="password" v-model="password" id="password" />
-    <button @click="register" type="submit">Register</button>
-  </div>
+    <div>
+        <h1>Register</h1>
+        <input type="email" name="email" v-model="email" id="email" />
+        <input
+            type="password"
+            name="password"
+            v-model="password"
+            id="password"
+        />
+        <div class="error" v-html="error"></div>
+        <button @click="register" type="submit">Register</button>
+    </div>
 </template>
 
 <script>
 import AuthenticationService from '@/services/AuthenticationService'
 export default {
-  data () {
-    return {
-      email: '',
-      password: '',
-      message: 'khong nam ma doi co an'
+    data () {
+        return {
+            email: '',
+            password: '',
+            error: null
+        }
+    },
+    methods: {
+        async register () {
+            try {
+                const response = await AuthenticationService.register({
+                    email: this.email,
+                    password: this.password
+                })
+                console.log(response)
+            } catch (e) {
+                this.error = e.response.data.error
+            }
+        }
     }
-  },
-  methods: {
-    async register () {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
-    }
-  }
 }
 </script>
 <style>
+.error {
+    color: red;
+}
 </style>
--- server/src/app.js
+++ server/src/app.js
@@ -1,18 +1,19 @@
-console.log('hello khong nam ma do co an thi chi co an duoi bau an shit thoy')
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const { sequelize } = require('./models')
+const config = require('./config/config')
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
+require ('./routers') (app)
 
-app.post('/register', (req, res)=>{
-    res.send({
-        message: `Hello ${req.body.email}! Your email was register! Have fun!`
-    })
+sequelize.sync()
+    .then(() => {
+    app.listen(config.port)
+    console.log(`Server started on port ${config.port}`)
 })
-app.listen(process.env.PORT || 8081)
